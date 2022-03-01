d1f39edd27c8ba3370972e988a43078ad652b766
nnhungjs <nnhungjs@gmail.com>
2022-03-01 09:22:43 +0700

    development

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
   },
 };
